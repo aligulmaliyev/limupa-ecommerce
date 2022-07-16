10e06f9c9134f6dc38232403c05e3dfa67897045
Ali Gulmaliyev <ali.gulmaliyov@gmail.com>
2022-07-17 01:42:25 +0400

    mini cart global img

--- src/containers/cart/MiniCart.js
+++ src/containers/cart/MiniCart.js
@@ -6,12 +6,12 @@ import { cartActions } from '../../store/slices/cart-slice';
 
 const MiniCart = () => {
     const cart = useSelector(state => state.cart);
-    const dispatch =useDispatch()
+    const dispatch = useDispatch()
     const [showMinicart, setShowMinicart] = useState(false)
     const handleMinicart = () => {
         setShowMinicart(perv => !perv)
     }
-    const removeFromCart=(id)=>{
+    const removeFromCart = (id) => {
         dispatch(cartActions.removeFromCart(id));
     }
     return (
@@ -44,17 +44,17 @@ const MiniCart = () => {
     )
 }
 
-const MiniCartItem = ({ data,removeFromCart }) => {
+const MiniCartItem = ({ data, removeFromCart }) => {
     return (
         <li>
             <Link to={`/product/${data?.id}`} className="minicart-product-image">
-                <img src={`assets/images/product/small-size/${data?.image}`} alt="cart products" />
+                <img src={process.env.PUBLIC_URL + `/assets/images/product/small-size/${data?.image}`} alt="cart products" />
             </Link>
             <div className="minicart-product-details">
                 <h6><Link to={`/product/${data?.id}`}>{data?.name}</Link></h6>
                 <span>${data?.totalPrice} x {data?.quantity}</span>
             </div>
-            <button onClick={()=>removeFromCart(data.id)} className="close" title="Remove">
+            <button onClick={() => removeFromCart(data.id)} className="close" title="Remove">
                 <i className="fa fa-close"></i>
             </button>
         </li>
