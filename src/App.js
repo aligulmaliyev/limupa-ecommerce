a93773ba754b8334844520e7a46e4e2dc1fbe7ec
Ali Gulmaliyev <ali.gulmaliyov@gmail.com>
2022-07-23 23:41:20 +0400

    lazyloading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layout/Layout';
-import About from './pages/About';
-import Cart from './pages/Cart';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Products from './pages/Products';
-import ProductsDetail from './pages/ProductsDetail';
-import WishList from './pages/WishList';
-import ScrollToTop from './components/ScrollToTop';
+const Layout = React.lazy(() => import("./layout/Layout"));
+const About = React.lazy(() => import("./pages/About"));
+const Cart = React.lazy(() => import("./pages/Cart"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Home = React.lazy(() => import("./pages/Home"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Products = React.lazy(() => import("./pages/Products"));
+const ProductsDetail = React.lazy(() => import("./pages/ProductsDetail"));
+const WishList = React.lazy(() => import("./pages/WishList"));
+const ScrollToTop = React.lazy(() => import("./components/ScrollToTop"));
 
 function App() {
   return (
