ae623d15aefbf6dd1c62b9fb4c1ba870d9eb26bf
Ali Gulmaliyev <ali.gulmaliyov@gmail.com>
2022-07-24 00:06:17 +0400

    minicart overlay

--- src/containers/cart/MiniCart.js
+++ src/containers/cart/MiniCart.js
@@ -11,32 +11,35 @@ const MiniCart = () => {
     }
 
     return (
-        <li className="hm-minicart">
-            <div className="hm-minicart-trigger is-active" onClick={handleMinicart}>
-                <span className="item-icon"></span>
-                <span className="item-text">${cart.subtotalPrice}
-                    <span className="cart-item-count">{cart.totalQuantity}</span>
-                </span>
-            </div>
-            <span></span>
-            {showMinicart && <div className='minicart'>
-                <ul className="minicart-product-list">
-                    {
-                        cart.cartItems?.slice(0, 3).map(cartItem => (
-                            <MiniCartItem key={cartItem.id} data={cartItem} />
-                        ))
-                    }
-                    {
-                        cart.totalQuantity === '0' && <li style={{ textAlign: 'center', display: 'block' }}>Empty Cart</li>
-                    }
-                </ul>
-                <p className="minicart-total">SUBTOTAL: <span>£{cart.subtotalPrice}</span></p>
-                <div className="minicart-button">
-                    <ButtonLink onClick={handleMinicart} type='dark' url='/cart' text='View Full Cart' />
-                    <ButtonLink onClick={handleMinicart} type='light' url='/checkout' text='Checkout' />
+        <>
+            {showMinicart && <div className='overlay' onClick={handleMinicart}></div>}
+            <li className="hm-minicart">
+                <div className="hm-minicart-trigger is-active" onClick={handleMinicart}>
+                    <span className="item-icon"></span>
+                    <span className="item-text">${cart.subtotalPrice}
+                        <span className="cart-item-count">{cart.totalQuantity}</span>
+                    </span>
                 </div>
-            </div>}
-        </li>
+                <span></span>
+                {showMinicart && <div className='minicart'>
+                    <ul className="minicart-product-list">
+                        {
+                            cart.cartItems?.slice(0, 3).map(cartItem => (
+                                <MiniCartItem key={cartItem.id} data={cartItem} />
+                            ))
+                        }
+                        {
+                            cart.totalQuantity === '0' && <li style={{ textAlign: 'center', display: 'block' }}>Empty Cart</li>
+                        }
+                    </ul>
+                    <p className="minicart-total">SUBTOTAL: <span>£{cart.subtotalPrice}</span></p>
+                    <div className="minicart-button">
+                        <ButtonLink onClick={handleMinicart} type='dark' url='/cart' text='View Full Cart' />
+                        <ButtonLink onClick={handleMinicart} type='light' url='/checkout' text='Checkout' />
+                    </div>
+                </div>}
+            </li>
+        </>
     )
 }
 
