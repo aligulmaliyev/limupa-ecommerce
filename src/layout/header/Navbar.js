cbb3611e2893edc12cdc2004dfa35ebb24a4a5f6
Ali Gulmaliyev <ali.gulmaliyov@gmail.com>
2022-07-24 15:54:45 +0400

    layout fix in cart

--- src/layout/header/Navbar.js
+++ src/layout/header/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-
+import { configActions } from '../../store/slices/config-slice';
 function Navbar() {
     const isActiveMenu = useSelector(state => state.config.isActiveMenu);
+    const dispatch = useDispatch();
+
     return (
         <div className={`header-bottom ${isActiveMenu ? 'active' : ''} header-sticky`}>
             <div className="container">
@@ -12,12 +14,12 @@ function Navbar() {
                         <div className={`hb-menu ${isActiveMenu ? 'mobile' : ''}`}>
                             <nav>
                                 <ul>
-                                    <li><NavLink end to="/" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Home</NavLink></li>
-                                    <li><NavLink end to="/products" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Shop</NavLink></li>
-                                    <li><NavLink end to="/products/categories/1" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Smartwatch</NavLink></li>
-                                    <li><NavLink end to="/products/categories/5" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Accessories</NavLink></li>
-                                    <li><NavLink end to="/about" className={({ isActive }) => isActive ? 'active-nav' : undefined}>About</NavLink></li>
-                                    <li><NavLink end to="/contact" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Contact</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Home</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/products" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Shop</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/products/categories/1" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Smartwatch</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/products/categories/5" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Accessories</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/about" className={({ isActive }) => isActive ? 'active-nav' : undefined}>About</NavLink></li>
+                                    <li><NavLink onClick={() => dispatch(configActions.menuToggle())} end to="/contact" className={({ isActive }) => isActive ? 'active-nav' : undefined}>Contact</NavLink></li>
                                 </ul>
                             </nav>
                         </div>
